e711d25d21538cc4b83b9481d2eed63f498fbb3e
Shrugsy <joshfraser91@gmail.com>
2020-01-26 13:07:42 +1100

    ready to deploy

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
-mongoose.connect(process.env.DBURL, {
+mongoose.connect(process.env.DBURL || 'mongodb://localhost:27017/warbler', {
     keepAlive: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
